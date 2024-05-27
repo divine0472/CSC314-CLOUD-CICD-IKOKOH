b7cd2710cf326d75ef983905b44a3d69041defe1
divine0472 <ikokohdivine@gmail.com>
2024-05-27 22:20:01 +0100

    Final commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' this is csc 314 cloud computing DEVOPS ; CI-CD PIPELINE !'));
+app.get('/', (req, res) => res.send(' THIS IS CSC314 CLOUD COMPUTING DEVOPS;CI-CD PIPELINE CREATED WITH AWS.THIS PAGE INDICATES SUCCESS NAME: IKOKOH DIVINE PRINCE  MATRIC NUMBER: VUG/CSC/21/6051'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
