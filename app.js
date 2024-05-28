e2dc7e9cf6bca081943cce16f59a71260cb00dcd
divine0472 <ikokohdivine@gmail.com>
2024-05-28 16:15:38 +0100

    fix

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' THIS IS CSC314 CLOUD COMPUTING DEVOPS;CI-CD PIPELINE CREATED WITH AWS.THIS PAGE INDICATES SUCCESS NAME: IKOKOH DIVINE PRINCE  MATRIC NUMBER: VUG/CSC/21/6051'));
+app.use(express.static("public"));
+// app.get('/', (req, res) => res.send(' THIS IS CSC314 CLOUD COMPUTING DEVOPS;CI-CD PIPELINE CREATED WITH AWS.THIS PAGE INDICATES SUCCESS NAME: IKOKOH DIVINE PRINCE  MATRIC NUMBER: VUG/CSC/21/6051'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
